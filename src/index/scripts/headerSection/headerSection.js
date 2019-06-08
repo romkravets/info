4b2a5697ef43a5c8adcc7ed7363debe89f4fd266
romkravets <romann.kravets@gmail.com>
2019-06-08 10:39:02 +0300

    Added dynemic slider in header  section

---
+++ src/index/scripts/headerSection/headerSection.js
@@ -0,0 +1,67 @@
+const ROOT_CLASS_NAMES = 'slider';
+const SLIDES__CLASS__ALL = `.${ROOT_CLASS_NAMES}__image`;
+const BUTTON_PREV_CLASS = `.${ROOT_CLASS_NAMES}__prev`;
+const BUTTON_NEXT_CLASS = `.${ROOT_CLASS_NAMES}__next`;
+const VISIBLE_CLASS = 'visible';
+
+export class HeaderSection {
+   constructor(rootElement, sliderData) {
+      this.rootElement = rootElement;
+         this.sliderData = sliderData;
+         this.slider;
+         this.buttonContainer;
+         this.buttonPrev;
+         this.buttonNext;
+         this.render();
+   }
+
+   render() {
+      this.slider = document.createElement('div');
+      this.slider.classList.add('slider__images');
+      this.rootElement.appendChild(this.slider);
+
+      for (let i = 0; i < this.sliderData.length; i++) {
+         const {img, name, desc} = this.sliderData[i];
+         const slideData = `
+           <div class="slider__image ${i == 0 ? 'visible' : ''}" data-slider="slide-${i}">
+             <div class="slide__image">
+              <img src="${img}" />
+             </div>
+           </div>
+         `;
+         this.slider.innerHTML += slideData;
+       }
+       const buttonContainer = `
+         <div class="slider__btns">
+            <input class="slider__prev" type="button" value="&#10094;"></input>
+            <input class="slider__next" type="button" value="&#10095;"></input>
+         </div>
+      `;
+      this.rootElement.innerHTML += buttonContainer;
+
+      this.buttonPrev = document.querySelector(BUTTON_PREV_CLASS);
+      this.buttonPrev.addEventListener('click', () => {
+         slides[currentSlide].classList.remove(VISIBLE_CLASS);
+         currentSlide--;
+         if(currentSlide < 0 )
+         currentSlide = slides.length - 1;
+         slides[currentSlide].classList.add(VISIBLE_CLASS);
+      })
+
+      this.buttonNext = document.querySelector(BUTTON_NEXT_CLASS);
+      this.buttonNext.addEventListener('click', () => {
+         slides[currentSlide].classList.remove(VISIBLE_CLASS);
+         currentSlide = (currentSlide + 1) % slides.length;
+         slides[currentSlide].classList.add(VISIBLE_CLASS);
+      })
+
+       const slides = document.querySelectorAll(SLIDES__CLASS__ALL);
+         let currentSlide = 0;
+         const slideInterval = setInterval(() => {
+         slides[currentSlide].classList.remove(VISIBLE_CLASS);
+         currentSlide = (currentSlide + 1) % slides.length;
+         slides[currentSlide].classList.add(VISIBLE_CLASS);
+         }, 7000);
+
+      }
+}
